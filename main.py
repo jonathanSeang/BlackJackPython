17f8b0f586d12aa88eff79d4a5ebc829287fb445
jonathanSeang <70104642+Jonstriker@users.noreply.github.com>
2021-06-11 08:12:48 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# BlackJack Project
+# Will have a dealer and human player
+# Uses a normal deck of cards
+# Player has limited amount of cash
+# Player can HIT or STAY to get to 21
+#     Maybe add INSURANCE, SPLIT, or DOUBLE DOWN
+# If player under 21, deal hits until they beat player or dealer busts
+
+# TODO:
+# - Create deck of 52 cards
+# - shuffle the deck
+# - ask player for their bet
+# - make sure player's bet does not exceed available chips
+# - deal two cards to the deal and two to player
+# - show only one of the dealer's cards, the other remains hidden
+# - show both of the player's cards
+# - ask the player if to hit
+# - if player doesn't bust, ask if the want to hit again
+# - if player stands, player the dealer's hand. Deal will always Hit until dealer's value meets or exceeds 17
+# - Determine winner and adjust Player's chips
+# - Ask player to play again
+
